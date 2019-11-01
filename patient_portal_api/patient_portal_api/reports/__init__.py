b0e368e0530ad615913d78736815cdc2ea061533
Liam <liam.j.lamb@gmail.com>
2019-11-01 11:54:22 +0000

    🔥  Removing currently redundant reports api

--- patient_portal_api/patient_portal_api/reports/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Module to expose the reports RESTful API"""
-
-from flask import Blueprint
-
-reports = Blueprint('reports', __name__)
-
-from . import routes
--- patient_portal_api/patient_portal_api/reports/routes.py
+++
@@ -1,8 +0,0 @@
-"""Module for handling the reports RESTful routing"""
-
-from . import reports
-
-@reports.route('/v1', methods=['GET'])
-def user_reports():
-    """Handle some report request"""
-    return "Hello, reports API!"
