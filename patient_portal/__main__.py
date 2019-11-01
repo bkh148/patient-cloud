536bb1091a59a4a9542d56f7e2dae9d5c473a844
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-01 16:08:59 +0000

    ✨ Error pages: dynamic css / js loading

--- patient_portal/__main__.py
+++ patient_portal/__main__.py
@@ -9,14 +9,14 @@ app = initialise_application("development")
 @app.errorhandler(404)
 def not_found_error(error):
     """Handling 404 errors"""
-    return render_template('404.html', title="Page Not Found", style_paths=
-            ['/static/css/error_page.css']), 404
+    return render_template('404.html', title="Page Not Found", static_folder='static', style_paths=
+                           ['css/error_page.css']), 404
 
 @app.errorhandler(500)
 def internal_server_error(error):
     """Handling 500 errors"""
-    return render_template('500.html', title="Internal Server Error", style_paths=
-            ['/static/css/error_page.css']), 500
+    return render_template('500.html', title="Internal Server Error", static_folder='static', style_paths=
+                           ['css/error_page.css']), 500
 
 
 if __name__ == '__main__':
