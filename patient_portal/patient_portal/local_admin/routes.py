f2a201a99ae321f4d811c5bcef1e67df86a5b1ca
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-04 00:19:14 +0000

    🚧  Adding support for local admin sidebar

--- patient_portal/patient_portal/local_admin/routes.py
+++ patient_portal/patient_portal/local_admin/routes.py
@@ -7,5 +7,18 @@ from flask import render_template
 @local_admin.route('/dashboard', methods=['GET'])
 def dashboard():
     """Handle the local admin dashboard"""
+
+    clinicians = {
+        "text": "Clinicians",
+        "url": "",
+        "style": "active",
+        "icon": "fas fa-user-md"}
+
+    appointments = {
+        "text": "Appointments",
+        "url": "",
+        "style": "",
+        "icon": "fas fa-calendar-check"}
+
     return render_template('local_admin/index.html', title='Dashboard - Local Admin', static_folder='local_admin.static', style_paths=
-                           ['css/main.css'])
+                           ['css/main.css'], nav_links=[clinicians, appointments])
