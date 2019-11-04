f975467273d29273517e6356f4a0c8384009163b
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-04 00:19:39 +0000

    🚧  Adding support for patient sidebar

--- patient_portal/patient_portal/patient/routes.py
+++ patient_portal/patient_portal/patient/routes.py
@@ -7,5 +7,12 @@ from flask import render_template
 @patient.route('/dashboard', methods=['GET'])
 def dashboard():
     """Handle the patient dashboard"""
+
+    appointments = {
+        "text": "Appointments",
+        "style": "active",
+        "url": "",
+        "icon": "fas fa-calendar-check"}
+
     return render_template('patient/index.html', title='Dashboard - Patient', static_folder='patient.static', style_paths=
-                           ['css/main.css'])
+                           ['css/main.css'], nav_links=[appointments])
