9eeb06a3da0e7dcd7c95ddecdd0f8b18318ed05c
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-04 00:18:15 +0000

    🚧  Adding support for admin sidebar

--- patient_portal/patient_portal/admin/routes.py
+++ patient_portal/patient_portal/admin/routes.py
@@ -8,6 +8,19 @@ from flask import render_template
 @admin.route('/dashboard', methods=['GET'])
 def dashboard():
     """Handle the admin dashboard"""
+
+    care_locations = {
+        "text": "Care Locations",
+        "style": "active",
+        "url": "",
+        "icon": "fas fa-hospital-alt"}
+
+    data_analytics = {
+        "text": "Analytics",
+        "style": "",
+        "url": "",
+        "icon": "fas fa-chart-line"}
+
     return render_template('admin/index.html', title='Dashboard - Admin', static_folder='admin.static', style_paths=
-                           ['css/main.css'])
+                           ['css/main.css'], nav_links=[care_locations, data_analytics])
 
