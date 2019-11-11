156b6975e3e845477b8fa7f603e6fd95000db021
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-11 14:28:16 +0000

    💩  Mock data comming in from services

--- patient_portal/patient_portal/patient/routes.py
+++ patient_portal/patient_portal/patient/routes.py
@@ -3,10 +3,25 @@
 from . import patient
 from flask import render_template
 
+from .. import services
+
 @patient.route('/', methods=['GET'])
 @patient.route('/dashboard', methods=['GET'])
 def dashboard():
     """Handle the patient dashboard"""
+
+    metadata = {}
+    metadata['appointments'] = {}
+
+    try:
+        appointments = services.appointment_service().get_appointments_for('0f837187-be84-4c4d-a3b6-745598174959')
+        metadata['appointments']['upcoming'] = appointments
+
+        print(metadata)
+
+    except Exception as e:
+        print("Some exception..")
+        raise e
 
     socket_config = {
         "host": "127.0.0.1",
@@ -29,4 +44,5 @@ def dashboard():
                            static_folder='patient.static',
                            style_paths=['css/main.css'],
                            nav_links=[dashboard, appointments],
-                           configurations=socket_config)
+                           configurations=socket_config,
+                           metadata=metadata)
