32677b56ce117de25c354ea9a02044de76f2ad61
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-11 14:30:37 +0000

    ♻  Refactore shared css

--- patient_portal/patient_portal/static/js/sidebar.js
+++ patient_portal/patient_portal/static/js/sidebar.js
@@ -1,11 +1,30 @@
+function update_icons() {
+    if ($('#sidebar').hasClass("active")) {
+	    $('#sidebar-collapse-icon').addClass('fa-angle-double-right').removeClass('fa-angle-double-left')
+    } else {
+	    $('#sidebar-collapse-icon').addClass('fa-angle-double-left').removeClass('fa-angle-double-right')
+    }
+}
+
+function update_sidebar() {
+    if ($(window).width() <= 768 && !$('#sidebar').hasClass('active')) {
+   	$('#sidebar').addClass('active')
+	$('#content').addClass('active')
+	update_icons()
+    }
+}
+
 $(document).ready(function () {
+    update_sidebar()
+
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
+	$('#content').toggleClass('active');
 
-	if ($('#sidebar').hasClass("active")) {
-		$('#sidebar-collapse-icon').addClass('fa-angle-double-right').removeClass('fa-angle-double-left')
-	} else {
-		$('#sidebar-collapse-icon').addClass('fa-angle-double-left').removeClass('fa-angle-double-right')
-	}
+	update_icons()
     });
+});
+
+$(window).resize(function() {
+    update_sidebar()
 });
