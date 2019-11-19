da537ffcaa58127e02c3ae0118aa5eff36bf111a
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-19 18:29:30 +0000

    🏗  Validating user from the user service

--- patient_portal/patient_portal/core/implementations/user_service.py
+++ patient_portal/patient_portal/core/implementations/user_service.py
@@ -1,9 +1,11 @@
 from ..interfaces import IUserService
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class UserService(IUserService):
     
-    def __init__(self, user_repo, log_service):
+    def __init__(self, user_repo, password_service, log_service):
         self._user_repo = user_repo
+        self._password_service = password_service
         self._log_service = log_service
         
     def get_user_by_id(self, user_id):
@@ -15,10 +17,33 @@ class UserService(IUserService):
             print("Service Exception {} : {}".format(__name__, e))
         
         return None;
+    
+    def validate_user(self, user_mail, user_password):
+        """ validate a user authentication request """ 
+        try:
+            user = self.get_user_by_email(user_mail)
+        
+            if user:
+                password = self._password_service.get_password_hash(user['user_id'])
+                if password and check_password_hash(password['password_hash'], user_password):
+                    return user
+                else:
+                    return None
+            else:
+                return None
+        except Exception as e:
+            print("Service Exception {} : {}".format(__name__, e))
+            
+        return None
             
     
     def get_user_by_email(self, user_mail):
         """ get a user object by the id """
+        try :
+            return self._user_repo.get_user_by_email(user_mail)
+        except Exception as e:
+            print("Service Exception {}: {}".format(__name__, e))
+        return None;
     
     def get_user_role_by_id(self, user_id):
         """ get a user's role by their id """
