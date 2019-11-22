084ac54414379f8dce2505d294fa1693fe96a3c8
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-22 10:08:33 +0000

    🔊  location service logs

--- patient_portal/patient_portal/core/implementations/location_service.py
+++ patient_portal/patient_portal/core/implementations/location_service.py
@@ -2,30 +2,38 @@ from ..interfaces import ILocationService
 
 class LocationService(ILocationService):
 
-    def __init__(self, loc_repo):
+    def __init__(self, loc_repo, log_service):
         self._location_repo = loc_repo
+        self._log_service = log_service
 
     def upsert_location(self, location):
         """update or insert a new location"""
-        self._location_repo.upsert_location(location)
+        try:
+            self._location_repo.upsert_location(location)
+        except Exception as e:
+            self._log_service.log_exception(e)
 
     def delete_location(self, location_id):
-        """ remove a location from the datastor """
-        self._location_repo.delete_location(location_id)
+        """ remove a location from the datastor """ 
+        try:
+            self._location_repo.delete_location(location_id)
+        except Exception as e:
+            self._log_service.log_exception(e)
 
     def get_location_by_id(self, location_id):
         """ return a given location object by its id """
-        return self._location_repo.get_location_by_id(location_id)
+        try:
+            return self._location_repo.get_location_by_id(location_id)
+        except Exception as e:
+            self._log_service.log_exception(e)
+        
+        return None
     
     def get_all_locations(self):
         """ returns all care location """
         try:
-            
-            # log activity
             return self._location_repo.get_all_locations();
-            
         except Exception as e:
-            # log exception
-            print("Service Exception | {} : {}".format(__name__, e))
+            self._log_service.log_exception(e)
         
         return []
