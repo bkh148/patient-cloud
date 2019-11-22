acb49568023a1faad9d212f64ad7a6d39c1301c8
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-22 10:14:52 +0000

    🔊  Exception log creation

--- patient_portal/patient_portal/core/implementations/log_service.py
+++ patient_portal/patient_portal/core/implementations/log_service.py
@@ -1,11 +1,37 @@
 """ Implementation of the log service abstraction """
-
+from flask import session
 from ..interfaces import ILogService
+import traceback, uuid
+from datetime import datetime
+from ..models.exception_types import ExceptionType
+from pprint import pprint as pp
 
 class LogService(ILogService):
 
     def __init__(self, repo):
         self._repo = repo
+
+    def stack_trace(self, exception):
+        stack = traceback.extract_stack()[:-3] + traceback.extract_tb(exception.__traceback__)  # add limit=?? 
+        pretty = traceback.format_list(stack)
+        return ''.join(pretty) + '\n  {} {}'.format(exception.__class__,exception)
+
+    def log_exception(self, error):
+        """build the exception object and log it"""
+        try:
+            stack_trace = self.stack_trace(error)
+            log = dict(exception_log_id=str(uuid.uuid4()),
+                   exception_log_type=ExceptionType.SERVICE_EXCEPTION.value,
+                   session_id=session['session_id'] if 'session_id' in session else str(uuid.UUID('00000000-0000-0000-0000-000000000000')),
+                   occurred_on_utc=datetime.utcnow(),
+                   is_handled = True,
+                   stack_trace = stack_trace)
+            
+            self.upsert_exception(log)
+            
+        except Exception as e:
+            print("Service Exception: {}".format(e))
+
 
     def get_exceptions_by_session_id(self, session_id):
         """get all exceptions from a given user session
@@ -40,6 +66,9 @@ class LogService(ILogService):
             empty [] or [] of activities.
         """
         return self._repo.get_activities_by_session_id(session_id)
+
+    def log_activity(self, error):
+        """Build the activity object and log it"""
 
     def upsert_activity(self, activity_log):
         """insert of update an activity log"""
