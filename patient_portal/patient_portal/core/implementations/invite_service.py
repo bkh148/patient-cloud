1bc507365dbb492e679bd5ee4eac23c2f5a42768
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-22 10:08:06 +0000

    🔊  Invite service logs

--- patient_portal/patient_portal/core/implementations/invite_service.py
+++ patient_portal/patient_portal/core/implementations/invite_service.py
@@ -1,5 +1,4 @@
 from ..interfaces import IInviteService
-import abc
 
 class InviteService(IInviteService):
     """
@@ -7,25 +6,41 @@ class InviteService(IInviteService):
 
     Todo: add service call logging
     """
-    def __init__(self, repo):
+    def __init__(self, repo, log_service):
         self._repo = repo
+        self._log_service = log_service
+        
 
     def upsert_invite(self, invite):
         """ update or insert a new invite into the table"""
         try:
             self._repo.upsert_invite(invite)
         except Exception as e:
-            print('Service Exception: {} : {}'.format(__name__, e))
+            self._log_service.log_exception(e)
             raise
 
     def delete_invite(self, invite_id):
         """ remove an invite from the table """
-        self._repo.delete_invite(invite_id)
+        try:
+            self._repo.delete_invite(invite_id)
+        except Exception as e:
+            self._log_service.log_exception(e)
+            raise
 
     def get_invites_created_by(self, user_id):
         """ get all invites created by a user"""
-        return self._repo.get_invites_created_by(user_id)
+        try:
+            return self._repo.get_invites_created_by(user_id)
+        except Exception as e:
+            self._log_service.log_exception(e)
+        
+        return []
 
     def get_invites_created_for(self, email):
         """ get all invites created for a user"""
-        return self._repo.get_invites_created_for(email)
+        try:
+            return self._repo.get_invites_created_for(email)
+        except Exception as e:
+            self._log_service.log_exception(e)
+        
+        return []
