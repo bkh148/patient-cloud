e1fe33e488ade04bc4ac6037717b32edc189fc99
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-22 10:13:43 +0000

    🏗  Appointment Repository API

--- patient_portal/patient_portal/data/implementations/appointment_repository.py
+++ patient_portal/patient_portal/data/implementations/appointment_repository.py
@@ -11,13 +11,12 @@ class AppointmentRepository(IAppointmentRepository):
         """Initialise"""
         self._db = db
 
-    def upsert_appointment(self, payload):
-        """ Updates or inserts a new appointment into the datastore.
-
-        Args:
-            payload: the appointment object.
-        """
-        return "upsert appointment"
+    def upsert_appointment(self, appointment):
+        """ Updates or inserts a new appointment into the datastore."""
+        if self.has_appointment(appointment['appointment_id']):
+            self._db.update('appointments', appointment)
+        else:
+            self._db.insert('appointments', appointment)
 
     def delete_appointment(self, appointment_id):
         """ Deletes an appointment form the datastore.
@@ -25,11 +24,15 @@ class AppointmentRepository(IAppointmentRepository):
         Args:
             appointment_id: id of the appointment.
         """
-        return "delete appointment"
+        self._db.execute("""
+            DELETE FROM appointments
+            WHERE appointment_id = ?""", (appointment_id, ))
 
     def has_appointment(self, appointment_id):
         """ Checks if an appointment exists. """
-        return "has appointment"
+        return self._db.count("""
+            SELECT COUNT() FROM appointments
+            WHERE appointment_id = ?""", (appointment_id, )) > 0
 
     def get_appointments_for(self, user_id):
         """ Get appointments created for a user.
@@ -37,59 +40,9 @@ class AppointmentRepository(IAppointmentRepository):
         Args:
             user_id: id of the user the appointments are for
         """
-
-        return [
-            {"appointment_id": "b7693c90-6009-4141-96be-a94cf62f3b84",
-             "created_by": "d2623b0a-2b48-49b9-82e3-8743f9254f78",
-             "created_for": "e1bbb1b3-9dfc-496d-a62c-c003646b9a46",
-             "location_name": "Astley Ainslie Hospital",
-             "location_coord_x": "1.21",
-             "location_coord_y": "1.21",
-             "location_postcode": "EH9 2HL",
-             "location_address": "Grange Loan",
-             "location_city": "Edinburgh",
-             "created_on_utc": datetime.utcnow(),
-             "appointment_date_utc": datetime.utcnow(),
-             "appointment_type": "Some check-up",
-             "appointment_notes": """
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-             """,
-             "is_cancelled": 0,
-             "is_attended": 0},
-            {"appointment_id": "",
-             "created_by": "d2623b0a-2b48-49b9-82e3-8743f9254f78",
-             "created_for": "e1bbb1b3-9dfc-496d-a62c-c003646b9a46",
-             "location_name": "Chalmers Hospital",
-             "location_coord_x": "1.21",
-             "location_coord_y": "1.21",
-             "location_postcode": "EH3 9HQ",
-             "location_address": "Chalmers Street",
-             "location_city": "Edinburgh",
-             "created_on_utc": datetime.utcnow(),
-             "appointment_date_utc": datetime.utcnow(),
-             "appointment_type": "Some check-up",
-             "appointment_notes": """
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-             """,
-             "is_cancelled": 0,
-             "is_attended": 0} ,
-           {"appointment_id": "",
-             "created_by": "d2623b0a-2b48-49b9-82e3-8743f9254f78",
-             "created_for": "e1bbb1b3-9dfc-496d-a62c-c003646b9a46",
-             "location_name": "Lauriston Building",
-             "location_coord_x": "1.21",
-             "location_coord_y": "1.21",
-             "location_postcode": "EH3 9EN",
-             "location_address": "Lauriston Place",
-             "location_city": "Edinburgh",
-             "created_on_utc": datetime.utcnow(),
-             "appointment_date_utc": datetime.utcnow(),
-             "appointment_type": "Some check-up",
-             "appointment_notes": """
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-             """,
-             "is_cancelled": 0,
-             "is_attended": 0} ]
+        return self._db.get_all("""
+        SELECT * FROM appointments
+        WHERE created_for = ?""", (user_id, ))
 
     def get_appointments_created_by(self, user_id):
         """ Get appointments create by a user.
@@ -97,7 +50,9 @@ class AppointmentRepository(IAppointmentRepository):
         Args:
             user_id: id of the user who created the appointments.
         """
-        return "get appointment created by"
+        return self._db.get_all("""
+        SELECT * FROM appointments
+        WHERE created_by = ?""", (user_id, ))
 
     def get_appointments_at_location(self, location_id):
         """ Get appointments at a given location.
@@ -105,4 +60,6 @@ class AppointmentRepository(IAppointmentRepository):
         Args:
             location_id: id of the location.
         """
-        return "get appointment at location"
+        return self._db.get_all("""
+        SELECT * FROM appointments
+        WHERE location_id = ?""", (location_id, ))
