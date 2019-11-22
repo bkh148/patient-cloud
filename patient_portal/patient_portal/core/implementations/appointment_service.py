77b64e4981de002107a1bdee88b7e0773c30028d
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-22 10:07:13 +0000

    🔊  Appointment service logs

--- patient_portal/patient_portal/core/implementations/appointment_service.py
+++ patient_portal/patient_portal/core/implementations/appointment_service.py
@@ -6,20 +6,66 @@ from ...data.interfaces import IAppointmentRepository
 class AppointmentService(IAppointmentService):
     """Implementation of the appointment service abstraction"""
 
-    def __init__(self, repo):
+    def __init__(self, repo, log_service):
         """ Some constructor """
         self._appointment_repository = repo
-        print("SERVICE : Appointment")
+        self._log_service = log_service
 
+    def upsert_appointment(self, appointment):
+        """ Create or Update an appointment """
+        try:
+            return []
+        except Exception as e:
+            self._log_service.log_exception(e)
+            raise
+        
+    def delete_appointment(self, appointment_id):
+        """Delete an existing appointment"""
+        try:
+            return []
+        except Exception as e:
+            self._log_service.log_exception(e)
+            raise
 
     def get_appointment(self, appointment_id):
         """ Get an appointment through the service
-
         Args:
             appointment_id: id of the app. to retrieve.
         """
-        return self._appointment_repository.get_appointments_created_for(appointment_id)
+        try:
+            raise Exception("Test exception!!")
+            return self._appointment_repository.get_appointments_created_for(appointment_id)
+        except Exception as e:
+            self._log_service.log_exception(e)
+            raise
+        
+        return None
 
     def get_appointments_for(self, user_id):
         """ Get all the appointments for a given user """
-        return self._appointment_repository.get_appointments_for(user_id)
+        try:
+            return self._appointment_repository.get_appointments_for(user_id)
+        except Exception as e:
+            self._log_service.log_exception(e)
+            raise
+        return []
+    
+    def get_appointments_created_by(self, user_id):
+        """ Get all the appointments for a given clinician"""
+        try:
+            return []
+        except Exception as e:
+            self._log_service.log_exception(e)
+            raise
+            
+        return []
+        
+    def get_appointments_at_location(self, user_id):
+        """ Get all the appointments for a given clinician"""
+        try:
+            return []
+        except Exception as e:
+            self._log_service.log_exception(e)
+            raise
+            
+        return []
