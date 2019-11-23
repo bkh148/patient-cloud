89c8cde9aa6e130e0f32e1790934d677ead3bd2b
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-23 12:05:54 +0000

    🚧  Improvements to appointments v1.0 api

--- patient_portal/patient_portal/api/appointments.py
+++ patient_portal/patient_portal/api/appointments.py
@@ -11,38 +11,14 @@ appointment_field = nsp.model('Appointment', {
     'appointment_id': fields.String(required=True,  description="The appointment's unique identifier."),
     'created_by': fields.String(required=True, description="Unique identifier of the user having created the appointment."),
     'created_for': fields.String(required=True, description="Unique identifier of the user this appointment was created for."),
-    'location': fields.Nested(location_fields),
-    'created_on_utc': fields.DateTime,
-    'appointment_date_utc': fields.DateTime,
-    'reason': fields.String(attribute='appointment_type'),
-    'notes': fields.String(attribute='appointment_notes'),
+    'location_id': fields.String(required=True),
+    'created_on_utc': fields.DateTime(required=True),
+    'appointment_date_utc': fields.DateTime(required=True),
+    'appointment_type': fields.String(required = True),
+    'appointment_notes': fields.String(required = True),
     'is_cancelled': fields.Boolean,
     'is_attended': fields.Boolean
 })
-
-mockappointment = {
-    "appointment_id": "4010b95e-4aba-4183-91f5-db9f13004f4f",
-    "created_by": "b4d74b21-2fc9-4567-8b16-59bc0a18fad2",
-    "created_for": "2088f26f-0700-4d1c-b4de-f93e9c71d5ee",
-    "location": {
-        "location_id": "d0e114ba-3527-4c4a-aee7-4bde6095f94f",
-        "location_name": "Lancaster's Hosptial",
-        "location_coord_x": 30.340271,
-        "location_coord_y": -152.409757,
-        "location_address": "953 Keap Street, 5093",
-        "location_postcode": "56",
-        "location_city": "Greenwich"
-    },
-    "created_on_utc": datetime.utcnow(),
-    "appointment_date_utc": datetime.utcnow(),
-    "appointment_type": "reprehenderit",
-    "appointment_notes": "nisi excepteur ut nulla reprehenderit veniam nostrud labore eu officia minim sit ut amet Lorem magna et velit eiusmod occaecat do laborum aliqua ipsum sit est labore excepteur eu dolor aute labore ullamco exercitation sint proident do quis occaecat est enim culpa et veniam proident est fugiat tempor labore mollit dolor consequat velit sint consectetur ex tempor ex ex laboris laboris excepteur tempor sunt nisi mollit deserunt et ea minim magna deserunt Lorem magna culpa qui id aliqua nostrud fugiat labore incididunt nulla enim laborum excepteur adipisicing esse Lorem sint exercitation exercitation reprehenderit Lorem cupidatat minim sit exercitation mollit laboris",
-    "is_cancelled": True,
-    "is_attended": False
-}
-
-
-appointments = [mockappointment]
 
 appointment_parser = reqparse.RequestParser()
 appointment_parser.add_argument('Authorization', type=str, required=True, location='headers', help="The user's access_token stored in the context manager.")
@@ -54,7 +30,6 @@ appointment_parser.add_argument('appointment_id', type=str, required=True)
 @nsp.response(500, 'Internal server error.')
 @nsp.response(403, 'Not authorized.')
 class Appointments(Resource):
-    #@nsp.doc(body=appointments_field)
     @nsp.marshal_list_with(appointment_field)
     def get(self):
         """ Get all appointments """
@@ -66,40 +41,41 @@ class Appointments(Resource):
     @nsp.doc(body=appointment_field, response={201: 'Appointment successfully created.'})
     def post(self):
         """ Post a new appointment """
-        appointment = request.json
-        return {"appointment": appointment}, 201
+        try:
+            appointment = request.json
+            services.appointment_service().upsert_appointment(appointment)
+            return {'message': 'Successfully added appointment.'}, 201
+        except Exception as e:
+            nsp.abort(500, "An internal error has occurred: {}".format(e))
+            
+@nsp.route('/clinician/<string:user_id>')
+@nsp.response(200, 'Appointments query executed successfully.')
+@nsp.response(400, 'Invalid appointment object.')
+@nsp.response(500, 'Internal server error.')
+@nsp.response(403, 'Not authorized.')
+class AppointmentsCreatedBy(Resource):
+    @nsp.marshal_with(appointment_field)
+    def get(self, user_id):
+        """Get all appointments created by a given user."""
+        try:
+            return services.appointment_service().get_appointments_created_by(user_id), 200
+        except Exception as e:
+            nsp.abort(500, "An internal error has occurred: {}".format(e))
         
+@nsp.route('/patient/<string:user_id>')
+@nsp.response(200, 'Appointments query executed successfully.')
+@nsp.response(400, 'Invalid appointment object.')
+@nsp.response(500, 'Internal server error.')
+@nsp.response(403, 'Not authorized.')
+class AppointmentsCreatedFor(Resource):
+    @nsp.marshal_with(appointment_field)
+    def get(self, user_id):
+        """Get all appointments created for a given user."""
+        try:
+            return services.appointment_service().get_appointments_for(user_id), 200
+        except Exception as e:
+            nsp.abort(500, "An internal error has occurred: {}".format(e))
 
-# @nsp.route('/<string:created_by>')
-# @nsp.response(200, 'Appointments query executed successfully.')
-# @nsp.response(400, 'Invalid appointment object.')
-# @nsp.response(500, 'Internal server error.')
-# @nsp.response(403, 'Not authorized.')
-# class AppointmentsCreatedBy(Resource):
-#     #@nsp.doc(body=appointments_field)
-#     @nsp.marshal_list_with(appointment_field)
-#     def get(self, created_by):
-#         """Get all appointments created by a given user."""
-#         try:
-#             return [], 200
-#         except Exception as e:
-#             nsp.abort(500, "An internal error has occurred: {}".format(e))
-
-# @nsp.route('/<string:created_for>')
-# @nsp.response(200, 'Appointments query executed successfully.')
-# @nsp.response(400, 'Invalid appointment object.')
-# @nsp.response(500, 'Internal server error.')
-# @nsp.response(403, 'Not authorized.')
-# class AppointmentsCreatedFor(Resource):
-#     #@nsp.doc(body=appointments_field)
-#     @nsp.marshal_list_with(appointment_field)
-#     def get(self, created_for):
-#         """Get all appointments created for a given user."""
-#         try:
-#             return [], 200
-#         except Exception as e:
-#             nsp.abort(500, "An internal error has occurred: {}".format(e))
-    
 # @nsp.route('/<string:location_id>')
 # @nsp.response(200, 'Appointments query executed successfully.')
 # @nsp.response(400, 'Invalid appointment object.')
