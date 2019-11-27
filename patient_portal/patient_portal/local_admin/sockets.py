ada6829836f27c8a0ff021fdd03a9fbed273b8e7
Liam Lamb <40271247@live.napier.ac.uk>
2019-11-27 21:07:07 +0000

    🏗  Local admin socket connect / disconnect

--- patient_portal/patient_portal/local_admin/sockets.py
+++ patient_portal/patient_portal/local_admin/sockets.py
@@ -1,9 +1,22 @@
 """Module for managing any local admin sockets"""
 
-from .. import socket_io
+from flask import session, request
 from flask_socketio import disconnect
-from .. import services
+from .. import socket_io, services, online_local_admins
 from ..core import authenticated_socket
+
+@socket_io.on('connect', namespace='/local_admin')
+def on_connect():
+    user_id = session['user']['user_id']
+    if user_id not in online_patients:
+        online_local_admins[user_id] = request.sid
+
+    
+@socket_io.on('disconnect', namespace='/local_admin')
+def on_disconnect():
+    user_id = session['user']['user_id']
+    if user_id in online_patients:
+        online_local_admins.pop(user_id)
 
 @socket_io.on('load_dashboard', namespace='/local_admin')
 @authenticated_socket
