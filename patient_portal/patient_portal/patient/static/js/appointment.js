f3a9e889de731ded84fb894ebece196e6ece1aa6
Liam Lamb <40271247@live.napier.ac.uk>
2019-12-03 21:02:26 +0000

    ♻  Standardise dictionnary access in JS

--- patient_portal/patient_portal/patient/static/js/appointment.js
+++ patient_portal/patient_portal/patient/static/js/appointment.js
@@ -1,19 +1,19 @@
-let build_appointment = function(appointment_json) {
+let build_appointment = function(appointment) {
     try {
         let appointment_wrapper = document.createElement('div')
-        appointment_wrapper.setAttribute('id', `appointment_wrapper_${appointment_json['appointment_id']}`)
-        appointment_wrapper.innerHTML = context_manager._cache.templates['appointments_item'];
+        appointment_wrapper.setAttribute('id', `appointment_wrapper_${appointment.appointment_id}`)
+        appointment_wrapper.innerHTML = context_manager._cache.templates.appointments_item;
 
         // Get and set the left date element
         let left_date = $(appointment_wrapper).find('#appointment_date_left');
-        let left_date_value = moment(appointment_json['appointment_date_utc']).format('dddd Do MMM YY');
-        $(left_date).attr('id', `${left_date.attr('id')}_${appointment_json['appointment_id']}`)
+        let left_date_value = moment(appointment.appointment_date_utc).format('dddd Do MMM YY');
+        $(left_date).attr('id', `${left_date.attr('id')}_${appointment.appointment_id}`)
         $(left_date).html(left_date_value)
 
         // Get and set the right date element
         let right_date = $(appointment_wrapper).find('#appointment_date_right');
-        let right_date_value = moment(appointment_json['appointment_date_utc']).format('hh:mm a');
-        $(right_date).attr('id', `${right_date.attr('id')}_${appointment_json['appointment_id']}`);
+        let right_date_value = moment(appointment.appointment_date_utc).format('hh:mm a');
+        $(right_date).attr('id', `${right_date.attr('id')}_${appointment.appointment_id}`);
         $(right_date).html(right_date_value)
 
 
@@ -22,7 +22,7 @@ let build_appointment = function(appointment_json) {
         for (let i = 0; i < context_manager._cache.locations.length; i++) {
             let temp = context_manager._cache.locations[i]
 
-            if (temp['location_id'] == appointment_json['location_id']) {
+            if (temp.location_id == appointment.location_id) {
                 location = temp;
                 break;
             }
@@ -33,28 +33,28 @@ let build_appointment = function(appointment_json) {
         }
 
         let location_name = $(appointment_wrapper).find('#appointment_location_name');
-        $(location_name).attr('id', `${location_name.attr('id')}_${appointment_json['appointment_id']}`);
-        $(location_name).html(location['location_name'])
+        $(location_name).attr('id', `${location_name.attr('id')}_${appointment.appointment_id}`);
+        $(location_name).html(location.location_name)
 
         let location_address = $(appointment_wrapper).find('#appointment_location_address');
-        $(location_address).attr('id', `${location_address.attr('id')}_${appointment_json['appointment_id']}`);
-        $(location_address).html(`${location['location_address']}, `)
+        $(location_address).attr('id', `${location_address.attr('id')}_${appointment.appointment_id}`);
+        $(location_address).html(`${location.location_address}, `)
 
         let location_postcode = $(appointment_wrapper).find('#appointment_location_postcode');
-        $(location_postcode).attr('id', `${location_postcode.attr('id')}_${appointment_json['appointment_id']}`);
-        $(location_postcode).html(`${location['location_postcode']}, `)
+        $(location_postcode).attr('id', `${location_postcode.attr('id')}_${appointment.appointment_id}`);
+        $(location_postcode).html(`${location.location_postcode}, `)
 
         let location_city = $(appointment_wrapper).find('#appointment_location_city');
-        $(location_city).attr('id', `${location_city.attr('id')}_${appointment_json['appointment_id']}`);
-        $(location_city).html(`${location['location_city']}, `)
+        $(location_city).attr('id', `${location_city.attr('id')}_${appointment.appointment_id}`);
+        $(location_city).html(`${location.location_city}, `)
 
         let appointment_reason = $(appointment_wrapper).find('#appointment_reason');
-        $(appointment_reason).attr('id', `${appointment_reason.attr('id')}_${appointment_json['appointment_id']}`);
-        $(appointment_reason).html(appointment_json['appointment_type'])
+        $(appointment_reason).attr('id', `${appointment_reason.attr('id')}_${appointment.appointment_id}`);
+        $(appointment_reason).html(appointment.appointment_type)
 
         let appointment_notes = $(appointment_wrapper).find('#appointment_notes');
-        $(appointment_notes).attr('id', `${appointment_notes.attr('id')}_${appointment_json['appointment_id']}`);
-        $(appointment_notes).html(appointment_json['appointment_notes'])
+        $(appointment_notes).attr('id', `${appointment_notes.attr('id')}_${appointment.appointment_id}`);
+        $(appointment_notes).html(appointment.appointment_notes)
 
 
         return appointment_wrapper
