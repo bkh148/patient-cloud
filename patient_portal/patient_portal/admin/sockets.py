f6d96d441eaf21ef38816ecfccc85053fc4c48c8
Liam Lamb <40271247@live.napier.ac.uk>
2019-12-05 22:14:05 +0000

    ♻ 📝  Refactor and document

--- patient_portal/patient_portal/admin/sockets.py
+++ patient_portal/patient_portal/admin/sockets.py
@@ -7,6 +7,7 @@ from ..core import authenticated_socket
 
 @socket_io.on('connect', namespace='/admin')
 def on_connect():
+    """On connect event with client's browser. This adds the the user's is and socket id to the in-memory cache."""
     user_id = session['user']['user_id']
     if user_id not in online_patients:
         online_admins[user_id] = request.sid
@@ -14,13 +15,12 @@ def on_connect():
     
 @socket_io.on('disconnect', namespace='/admin')
 def on_disconnect():
+    """On disconnect event with client's browser. This removes the the user's is and socket id from the in-memory cache."""
     user_id = session['user']['user_id']
     if user_id in online_patients:
         online_admins.pop(user_id)
 
 @socket_io.on('logout', namespace='/admin')
 def logout(message):
-    # To do: tidy up work to remove client from any necessary rooms
-    print('Socket closed through logout')
     disconnect()
     
