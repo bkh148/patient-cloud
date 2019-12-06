dd36a827a6c4d6b2fb0c62012f7995a2f915c47f
Liam Lamb <40271247@live.napier.ac.uk>
2019-12-06 01:31:25 +0000

    🐛  HOST_NAME on main

--- patient_portal/__main__.py
+++ patient_portal/__main__.py
@@ -4,5 +4,5 @@ from flask import render_template
 from start_up import app, socket_io
 
 if __name__ == '__main__':
-    socket_io.run(app, host=app.config['HOST_NAME'],
+    socket_io.run(app, host=app.config['HOST'],
                   port=app.config['PORT'], log_output=True)
